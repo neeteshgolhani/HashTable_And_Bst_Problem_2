5da1f318436f28b82dac6dbabed805738d4017f5
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-05-04 11:13:02 +0530

    Added:Remove avoidabble from the paragraph

--- src/com/bridgelabz/HashTableMain.java
+++ src/com/bridgelabz/HashTableMain.java
@@ -1,13 +1,14 @@
 package com.bridgelabz;
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 public class HashTableMain {
     // Define the main method
     public static void main(String[] args) {
-        // Create a sentence string
-        String sentence = "To be or not to be";
-
-        // Split the sentence string into an array of words
-        String[] word_list = sentence.split(" ");
+        // Create a para string
+        String para = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+        // Split the para string into an array of words
+        String[] word_list = para.split(" ");
 
         // Create a new hash table with the same length as the word array
         MyHashTable hash_table = new MyHashTable(word_list.length);
@@ -16,14 +17,16 @@ public class HashTableMain {
         for (String word : word_list) {
             hash_table.add(word, 1);
         }
+        // Remove the word "avoidable" from the hash table
+        hash_table.remove("avoidable");
+        // Get the list of words and sort them alphabetically
+        List<String> word_list_sorted = new ArrayList<>(hash_table.getWords());
+        Collections.sort(word_list_sorted);
 
-        // Loop through the word array again
-        for (String word : word_list) {
-            // Get the frequency of the word in the hash table
+        // Print out the frequencies of the words
+        for (String word : word_list_sorted) {
             int frequency = hash_table.getFrequency(word);
-
-            // Print the word and its frequency to the console
-            System.out.printf("The word '%s' appears %d times in the sentence.\n", word, frequency);
+            System.out.printf("The frequency of word '%s' is %d\n", word, frequency);
         }
     }
 }

161a8d7952c9c50fd22511c4e981a4db436c8218
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-05-04 16:14:20 +0530

    Added:Find frequency of all words from paragarph

--- src/com/bridgelabz/HashTableMain.java
+++ src/com/bridgelabz/HashTableMain.java
@@ -1,7 +1,5 @@
 package com.bridgelabz;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+
 public class HashTableMain {
     // Define the main method
     public static void main(String[] args) {
@@ -12,21 +10,18 @@ public class HashTableMain {
 
         // Create a new hash table with the same length as the word array
         MyHashTable hash_table = new MyHashTable(word_list.length);
-
-        // Loop through the word array and add each word to the hash table with a value of 1
+// Loop through the word array and add each word to the hash table with a value of 1
         for (String word : word_list) {
             hash_table.add(word, 1);
         }
-        // Remove the word "avoidable" from the hash table
-        hash_table.remove("avoidable");
-        // Get the list of words and sort them alphabetically
-        List<String> word_list_sorted = new ArrayList<>(hash_table.getWords());
-        Collections.sort(word_list_sorted);
 
-        // Print out the frequencies of the words
-        for (String word : word_list_sorted) {
+        // Loop through the word array again
+        for (String word : word_list) {
+            // Get the frequency of the word in the hash table
             int frequency = hash_table.getFrequency(word);
-            System.out.printf("The frequency of word '%s' is %d\n", word, frequency);
+
+            // Print the word and its frequency to the console
+            System.out.printf("The word '%s' appears %d times in the sentence.\n", word, frequency);
         }
     }
 }
--- src/com/bridgelabz/MyHashTable.java
+++ src/com/bridgelabz/MyHashTable.java
@@ -1,6 +1,5 @@
 package com.bridgelabz;
-import java.util.List;
-import java.util.ArrayList;
+
 public class MyHashTable {
     // Define class variables
     int bucket_size;
@@ -35,61 +34,38 @@ public class MyHashTable {
 
     // Define the getFrequency method to get the frequency of a key in the hash table
     public int getFrequency(String key) {
-        // get the hash key for the given key
+        // Get the hash value of the key
         int hash_key = this.getHash(key);
-        // get the head of the linked list at the bucket corresponding to the hash key
+
+        // Loop through the linked list in the corresponding bucket and return the value of
+        // the key if it is found
         MyMapNode node = this.buckets[hash_key].head;
-        // traverse the linked list to find the node with the given key
         while (node != null) {
             if (node.key.equals(key)) {
-                // return the value of the node if the key matches
                 return node.value;
             }
             node = node.next;
         }
-        // return 0 if the key is not found in the hash table
+
+        // Return 0 if the key is not found
         return 0;
     }
-
-    public List<String> getWords() {
-        // create a new ArrayList to hold the words in the hash table
-        List<String> words = new ArrayList<>();
-        // iterate over each bucket in the hash table
+    @Override
+    public String toString() {
+        // initialize a StringBuilder to store the resulting string
+        StringBuilder sb = new StringBuilder();
+        // loop through each bucket in the hash table
         for (LinkedList bucket : this.buckets) {
-            // get the head of the linked list at the current bucket
-            MyMapNode node = bucket.head;
-            // traverse the linked list to add each word to the words ArrayList
-            while (node != null) {
-                words.add(node.key);
-                node = node.next;
+            // loop through each node in the linked list for the current bucket
+            MyMapNode currentNode = bucket.head;
+            while (currentNode != null) {
+                // append the key of the current node to the StringBuilder
+                sb.append(currentNode.key).append(" ");
+                // move to the next node in the linked list
+                currentNode = currentNode.next;
             }
         }
-        // return the words ArrayList
-        return words;
-    }
-
-    public void remove(String key) {
-        // get the hash key for the given key
-        int hashKey = this.getHash(key);
-        // initialize variables for previous node and current node
-        MyMapNode prevNode = null;
-        MyMapNode currentNode = this.buckets[hashKey].head;
-        // loop through the linked list until the end or until the node with the given key is found
-        while (currentNode != null) {
-            if (currentNode.key.equals(key)) {
-                // if the node to remove is the head of the linked list
-                if (prevNode == null) {
-                    this.buckets[hashKey].head = currentNode.next;
-                } else {
-                    // if the node to remove is not the head of the linked list
-                    prevNode.next = currentNode.next;
-                }
-                // exit the method after removing the node
-                return;
-            }
-            // update previous and current nodes
-            prevNode = currentNode;
-            currentNode = currentNode.next;
-        }
+        // return the resulting string, trimmed of any trailing whitespace
+        return sb.toString().trim();
     }
 }
